c49534269edbb18cf764332b78ea5c78dd158b58
Flyinlow39 <31428574+Flyinlow39@users.noreply.github.com>
2018-11-19 18:35:18 -0800

    Create .sol

---
+++ .sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.25;
+
+contract CollectorAircraftRegistry {
+
+string url;
+string name;
+uint timestamp;
+
+bool  newBool = true;
+
+int8 newInt = 2;
+
+uint[10] newIntArray = [1,2,3];
+
+address newAddress = msg.sender;
+
+function getBalance () {
+    newAddress.balance;
+    newAddress.transfer
+    
+}
+}
